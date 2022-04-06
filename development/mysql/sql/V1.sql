2a43ed6e4a34251c2f3e7c50cf650363aa900bc2
k-konno <k_konno@dreamarts.co.jp>
2022-04-06 16:01:46 +0900

    index追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,2 +1,4 @@
 create index session_idx on session (value);
 create index record_status_idx on record (status);
+create index record_idx on record (record_id);
+create index linked_record_id_idx on record_item_file (linked_record_id);
