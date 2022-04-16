08b650fef0aee57b01bf60e689033c398bcc2926
bayamasa <mapi0213mgt@gmail.com>
2022-04-16 19:06:59 +0900

    done

--- development/mysql/sql/V0.sql
+++ development/mysql/sql/V0.sql
@@ -84,8 +84,3 @@ CREATE TABLE `file` (
     `name` VARCHAR(256) NOT NULL,
     PRIMARY KEY (`file_id`)
 );
-
-ALTER TABLE record_comment ADD INDEX index_1(value);
-ALTER TABLE record ADD INDEX index_2(value);
-ALTER TABLE record_item_file ADD INDEX index_3(value);
-ALTER TABLE session ADD INDEX indedx_4(value);
---
+++ development/mysql/sql/V1.sql
@@ -0,0 +1,4 @@
+ALTER TABLE record_comment ADD INDEX index_1(value);
+ALTER TABLE record ADD INDEX index_2(value);
+ALTER TABLE record_item_file ADD INDEX index_3(value);
+ALTER TABLE session ADD INDEX index_4(value);
